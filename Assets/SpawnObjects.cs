bda997ddb5a8442207ef383b4cc42bf0ca7a8e24
Its-a-me-Mirs <61257890+Its-a-me-Mirs@users.noreply.github.com>
2022-02-14 15:45:24 -0800

    setting up limiting generation around camera

--- Assets/SpawnObjects.cs
+++ Assets/SpawnObjects.cs
@@ -6,30 +6,37 @@ public class SpawnObjects : MonoBehaviour
 {
     // NOTE: I haven't set it up for the script to reload when public values change in the editor so for now it'll have to be changed manually here
     
-    // if number of type of tagged items increase, just add more values to the two array here
+    // tagged items cutomized values goes here, switches to defaultVals if array is not long enough (below)
+   
     // spread of object spwan from -value[i] to value[i]
     public float[] radiusVals = {25, 50, 75, 60};
     // number of the given object to spawn
     public float[] numOfObjects = {50, 75, 100, 250};
 
-    // stores the types of object
-    GameObject[] objs;
-    int counter = 0;
-
-    // setting up defalut value if outside array length
+    // setting up default value if outside array length
     public float pass_default = 50;
     float passerRange = 0;
     float passerNums = 0;
+
+    // stores the types of object
+    GameObject[] objs;
+    int counter = 0;
 
     void Start() {
         objs = GameObject.FindGameObjectsWithTag("spawn");
         //int numObjects = objs.Length;
         //Debug.Log(numObjects);
+        
+        cam = GameObject.FindGameObjectWithTag("MainCamera");
+        //Debug.Log("camera y " + cam.transform.position.y);
     }
 
     void Update() {
-        // for each of the types of object, will create the number and spread specified
+        // TO-DO: restricting the Random.Range from including a specific area around the camera
+
+        // for each of the types of object, will create the number of and spread specified
         foreach(GameObject g in objs){
+
             // use value at the index if not null [radiusVals]
             if (System.Array.IndexOf(objs, g) < radiusVals.Length){
                 passerRange = radiusVals[System.Array.IndexOf(objs, g)];
