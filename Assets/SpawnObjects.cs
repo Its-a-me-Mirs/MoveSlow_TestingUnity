ba11e5a1c63416f41404ce6484578aa2f868231b
Its-a-me-Mirs <61257890+Its-a-me-Mirs@users.noreply.github.com>
2022-02-16 18:15:48 -0800

    need to set up array's object ordering

--- Assets/SpawnObjects.cs
+++ Assets/SpawnObjects.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class SpawnObjects : MonoBehaviour
 {
-    // NOTE: I haven't set it up for the script to reload when public values change in the editor so for now it'll have to be changed manually here
+    // NOTE: I haven't set it up for the script to re-exictute for now you'll have to turn on and off play mode
     
     // tagged items cutomized values goes here, switches to defaultVals if array is not long enough (below)
    
@@ -23,24 +23,26 @@ public class SpawnObjects : MonoBehaviour
     GameObject[] objs;
     int counter = 0;
 
-    // no spawn area size
-    float noSpawnVal = 15;
+    // setting up no spawn area size (around center of the spawn)
+    public float noSpawnVal = 10;
     float holderX, holderZ = 0;
     float camX, camZ = 0;
 
     void Start() {
+        // TO-DO: set up re-ordering of the array of objects
+
         objs = GameObject.FindGameObjectsWithTag("spawn");
         //int numObjects = objs.Length;
         //Debug.Log(numObjects);
         
+        // the generation will follow wherever the x and z positions are... could change it to a seperate empty object is needed
         Camera cam = GameObject.FindWithTag("MainCamera").GetComponent<Camera>();
         camX = cam.transform.position.x;
         camZ = cam.transform.position.z;
     }
 
     void Update() {
-        // TO-DO: restricting the Random.Range from including a specific area around the camera
-        
+    
         // for each of the types of object, will create the number of and spread specified
         foreach(GameObject g in objs){
             // index of the current object
@@ -59,70 +61,37 @@ public class SpawnObjects : MonoBehaviour
                 passerNums = numOfObjects[System.Array.IndexOf(objs, g)];
             } else { passerNums = pass_default;}
 
-            // To-DO: random range based on area around camera and noSpawn buffer space
-            //create logic based on the 4 quadrants
-
-            // if (Random.Range(0,2) == 0){
-            //     holderX = Random.Range(passerRange, camX + noSpawnVal);
-            // } else {
-            //     holderX = Random.Range(-passerRange, camX - noSpawnVal);
-            // }
-
-            // if (Random.Range(0,2) == 0){
-            //     holderZ = Random.Range(passerRange, camZ + noSpawnVal);
-            // } else {
-            //     holderZ = Random.Range(-passerRange, camZ - noSpawnVal);
-            // }
-
-            // if (Random.Range(0,4) == 0){
-            //     //Quadrant 1
-            //     holderX = Random.Range(camX + noSpawnVal, passerRange);
-            //     holderZ = Random.Range(camZ + noSpawnVal, passerRange);
-            // }
-            // else if (Random.Range(0,4) == 1){
-            //     //Quadrant 2
-            //     holderX = Random.Range(camX + noSpawnVal, passerRange);
-            //     holderZ = Random.Range(camZ - noSpawnVal, -passerRange);
-            // }
-            // else if (Random.Range(0,4) == 2){
-            //     //Quadrant 3
-            //     holderX = Random.Range(camX - noSpawnVal, -passerRange);
-            //     holderZ = Random.Range(camZ - noSpawnVal, -passerRange);
-            // }
-            // else {
-            //     // Quadrant 4
-            //     holderX = Random.Range(camX - noSpawnVal, -passerRange);
-            //     holderZ = Random.Range(camZ + noSpawnVal, passerRange);
-            // }
-
-            if (Random.Range(0,4) == 0){
+            // random range based on area around camera and noSpawn buffer space
+            // evenly distributes across all 4 quadrants (instead of randomly picking quadrants)
+            if (counter % 4 == 0){
                 // x is greater than noSpawn
-                holderX = Random.Range(camX + noSpawnVal, passerRange);
-                holderZ = Random.Range(-passerRange, passerRange);
+                holderX = Random.Range(camX + noSpawnVal, camX + passerRange);
+                holderZ = Random.Range(-passerRange + camZ, passerRange + camZ);
             }
-            else if (Random.Range(0,4) == 1){
+            else if (counter % 4 == 1){
                 // x is less than -noSpawn
-                holderX = Random.Range(camX - noSpawnVal, -passerRange);
-                holderZ = Random.Range(-passerRange, passerRange);
+                holderX = Random.Range(camX - noSpawnVal, -passerRange + camX);
+                holderZ = Random.Range(-passerRange + camZ, passerRange + camZ);
             }
-            else if (Random.Range(0,4) == 2){
+            else if (counter % 4 == 2){
                 // z is greater than noSpawn
-                holderZ = Random.Range(camZ + noSpawnVal, passerRange);
-                holderX = Random.Range(-passerRange, passerRange);
+                holderZ = Random.Range(camZ + noSpawnVal, camZ + passerRange);
+                holderX = Random.Range(-passerRange + camX, passerRange + camX);
             }
             else {
                 // z is less than noSpawn
-                holderZ = Random.Range(camZ - noSpawnVal, -passerRange);
-                holderX = Random.Range(-passerRange, passerRange);
+                holderZ = Random.Range(camZ - noSpawnVal, -passerRange + camZ);
+                holderX = Random.Range(-passerRange + camX, passerRange + camX);
             }
 
             // number of objects to spawn for each type of object
             if (counter <= passerNums){
-                // creates instances of the object into the scene with random position within the specified range
+                // creates instances of the object into the scene with the specified ranges
                 // Simplified Translation:
-                // Instantiate(object to make, Position(random X range, y=0, random Z range), Rotation)
-                Debug.Log("passerRange: " + passerRange + " holderX: " + holderX + " holderZ: " + holderZ);
-                GameObject.Instantiate(g.gameObject, new Vector3(holderX, 0, holderZ), Quaternion.Euler(new Vector3(0f, 90f, 0f)));
+                // Instantiate(object to make, Position(random X range, y=0, random Z range), Rotation (x=0, random y axis, z=0))
+                
+                //Debug.Log("passerRange: " + passerRange + " holderX: " + holderX + " holderZ: " + holderZ);
+                GameObject.Instantiate(g.gameObject, new Vector3(holderX, 0, holderZ), Quaternion.Euler(new Vector3(0f, Random.Range(0f,360f), 0f)));
                 
                 counter++;
             }
